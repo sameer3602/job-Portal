762149b3414b25659802dc964517647741b86563
Sameer Ali <sameerkhan.1234asd@gmail.com>
2024-09-04 02:24:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobportal-frontend-94p4.onrender.com/',
+    origin:'https://jobportal-frontend-94p4.onrender.com',
     credentials:true
 }
 
